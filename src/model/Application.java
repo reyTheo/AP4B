0fbcb918150739c4fcd2de6bfd089f698da2ac0d
Theo Rey <reytheopro@gmail.com>
2024-05-21 17:56:47 +0200

    change file data

--- src/model/Application.java
+++ src/model/Application.java
@@ -50,7 +50,7 @@ public class Application {
 	public final static float RETOUR_ZOOM_INITIAL = -2;
 	
 	/** The DOSSIE r_ images. */
-	private final String DOSSIER_IMAGES = "img/";
+	private final String DOSSIER_IMAGES = "data/";
 	
 	/** The ECHELL e_ carte. */
 	private final double ECHELLE_CARTE = 7.5; // <ECHELLE_CARTE> metres = 1 px
